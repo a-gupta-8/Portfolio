4174ee8750723c44ec1fa8023b153efedd442961
a-gupta-8 <avibox9511051@gmail.com>
2025-03-13 06:38:51 -0700

    zoom in/out of object with scrollEvent

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -4,6 +4,28 @@ import { useRef, useEffect, useState } from "react"
 import * as THREE from "three";
 const Cube = ({position, size}: {position: Array<number>, size: Array<number>}) => {
   const ref = useRef<THREE.Mesh>(null)
+  const scrollSpeed = 0.1;
+
+  useEffect(() => {
+    const handleScroll = (event: WheelEvent) => {
+      if (ref.current) {
+        if (ref.current.position.z - (event.deltaY * scrollSpeed * 0.01) < -5.0) {
+          ref.current.position.z = -5.0
+        }
+        else if (ref.current.position.z - (event.deltaY * scrollSpeed * 0.01) > 3.5) {
+          ref.current.position.z = 3.5
+        }
+        else {
+          ref.current.position.z -= event.deltaY * scrollSpeed * 0.01;
+        }
+        
+        console.log(ref.current.position.z)
+      }
+    };
+
+    window.addEventListener("wheel", handleScroll);
+    return () => window.removeEventListener("wheel", handleScroll);
+  }, []);
 
   useFrame((state, delta) => {
       ref.current.rotation.x += delta / 2
